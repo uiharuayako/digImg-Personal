81d858312629b3b6187a66a99dd62c992d5503bf
uiharuayako <2451602772@qq.com>
2021-01-12 15:17:41 +0800

    添加必要资源

--- digImg-Personal.cpp
+++ digImg-Personal.cpp
@@ -2,18 +2,40 @@
 //
 
 #include <iostream>
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat img = imread("ik_beijing_c.bmp");
+
+	// unsigned char* ptr = img.data;
+
+	// 获取图像的宽高
+	int height, width;
+	height = img.rows;
+	width = img.cols;
+
+	// 创建灰度图
+	Mat grayImg = Mat(height, width, CV_8UC1);
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			Vec3b px = img.at<Vec3b>(i, j);
+			grayImg.at<uchar>(i, j) = px[0] / 3 + px[1] / 3 + px[2] / 3;
+		}
+	}
+
+	// 输出
+	namedWindow("灰度图", 0);
+	imshow("灰度图", grayImg);
+	waitKey();
+
+	getchar();
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
